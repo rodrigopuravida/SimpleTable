8cf8fbfe810005fa9e1ef27197f133435d975ade
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2018-04-23 22:47:38 -0700

    Added images for restaurants

--- SimpleTable/ViewController.swift
+++ SimpleTable/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia","Royal Oak", "CASK Pub and Kitchen"]
     
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "PetiteOyster", "ForKeeRestaurant", "PosAtelier", "BourkeStreetBakery", "HaighsChocolate", "PalominoEspresso", "Upstate", "Traif", "GrahamAvenueMeats", "WaffleWolf", "FiveLeaves", "CafeLore", "Confessional", "Barrafina", "Donostia","RoyalOak", "CASKPubKitchen"]
+    
     
     
     
@@ -26,6 +28,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         //configure the cell
         cell.textLabel?.text = restaurantNames[indexPath.row]
         
+        cell.imageView?.image = UIImage(named: restaurantImages[indexPath.row].lowercased())
+        
         return cell
     }
     
@@ -38,6 +42,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
 
 
